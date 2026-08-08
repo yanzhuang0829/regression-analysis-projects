9dce56266da4914d9079dfc41144a92afa2fbe99
Yan Z <yanzhuang900829@gmail.com>
2026-08-08 10:55:49 -0400

    Delete student-dropout-prediction/Admission-Target h0h1.R

--- student-dropout-prediction/Admission-Target h0h1.R
+++
@@ -1,8 +0,0 @@
-if (!require("tidyverse")) install.packages("tidyverse", dependencies = TRUE)
-library(tidyverse)
-dropout_admission <- data$Admission_grade[data$Target == 'Dropout']
-graduate_admission <- data$Admission_grade[data$Target == 'Graduate']
-
-t_test_result <- t.test(dropout_admission, graduate_admission, alternative = "two.sided", na.rm = TRUE)
-
-print(t_test_result)
